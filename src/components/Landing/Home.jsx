be1b69e15a79c57a79241244f84a5aa326bc29b8
Prerak Gada <prerakgada07@gmail.com>
2023-02-04 22:22:16 +0530

    New UI

--- src/components/Landing/Home.jsx
+++ src/components/Landing/Home.jsx
@@ -6,13 +6,10 @@ import TopNav from "../TopNav/TopNav";
 export class Home extends Component {
 	
 	render() {
-		const items = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		const items = [1, 2, 3, 4, 5];
 		return (
 			<div>
 				<body>
-				
-				<TopNav/>
-				<SideBar/>
 				
 				<div className="main-container">
 					
