62baa7733ddd15e2adf6496157919fe6df31ad3c
Jhenil Parihar <jhenilparihar@gmail.com>
2023-02-04 12:45:30 +0530

    initial

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const NFTMarketplace = artifacts.require("Marketplace");
-
-module.exports = async function(deployer) {
-  await deployer.deploy(NFTMarketplace);
-};
--- src/components/Loading/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import loadingGIF from "./metamask.gif";
-
-const Loading = () => {
-  return <img src={loadingGIF} alt="Loading.." className="d-block m-auto" />;
-};
-
-export default Loading;
